99a3140d0710ec26a8b72b724daa76be2c92360b
‪ron dahan‬‏ <80279923+rondahan@users.noreply.github.com>
2025-05-11 11:59:24 +0300

    Delete card.js

--- card.js
+++
@@ -1,2 +0,0 @@
-export function Card({ children }) { return <div className="rounded-xl shadow bg-white">{children}</div>; }
-export function CardContent({ children, className }) { return <div className={className}>{children}</div>; }
